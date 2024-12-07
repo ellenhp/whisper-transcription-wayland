842f1455e13dbb16505a791f3ec35e542019d72e
Ellen Poe <ellen@ellenhp.me>
2024-12-07 01:38:34 +0000

    initial commit

---
+++ src/audio.rs
@@ -0,0 +1,121 @@
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{FromSample, Sample};
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+
+pub fn record_wav<P: AsRef<Path>>(path: P) -> Result<(), anyhow::Error> {
+
+    let host = cpal::default_host();
+
+    // Set up the input device and stream with the default input config.
+    let device = host.default_input_device()
+        .expect("failed to find input device");
+
+    println!("Input device: {}", device.name()?);
+
+    let config = device
+        .default_input_config()
+        .expect("Failed to get default input config");
+    println!("Default input config: {:?}", config);
+
+    let spec = wav_spec_from_config(&config);
+    let writer = hound::WavWriter::create(path, spec)?;
+    let writer = Arc::new(Mutex::new(Some(writer)));
+
+    // A flag to indicate that recording is in progress.
+    println!("Begin recording...");
+
+    // Run the input stream on a separate thread.
+    let writer_2 = writer.clone();
+
+    let err_fn = move |err| {
+        eprintln!("an error occurred on stream: {}", err);
+    };
+
+    let stream = match config.sample_format() {
+        cpal::SampleFormat::I8 => device.build_input_stream(
+            &config.into(),
+            move |data, _: &_| write_input_data::<i8, i8>(data, &writer_2),
+            err_fn,
+            None,
+        )?,
+        cpal::SampleFormat::I16 => device.build_input_stream(
+            &config.into(),
+            move |data, _: &_| write_input_data::<i16, i16>(data, &writer_2),
+            err_fn,
+            None,
+        )?,
+        cpal::SampleFormat::I32 => device.build_input_stream(
+            &config.into(),
+            move |data, _: &_| write_input_data::<i32, i32>(data, &writer_2),
+            err_fn,
+            None,
+        )?,
+        cpal::SampleFormat::F32 => device.build_input_stream(
+            &config.into(),
+            move |data, _: &_| write_input_data::<f32, f32>(data, &writer_2),
+            err_fn,
+            None,
+        )?,
+        sample_format => {
+            return Err(anyhow::Error::msg(format!(
+                "Unsupported sample format '{sample_format}'"
+            )))
+        }
+    };
+
+    stream.play()?;
+
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
+    
+    while running.load(Ordering::SeqCst) {
+        std::thread::sleep(std::time::Duration::from_millis(10));
+    }
+
+    drop(stream);
+    writer.lock().unwrap().take().unwrap().finalize()?;
+
+    Ok(())
+}
+
+fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
+    if format.is_float() {
+        hound::SampleFormat::Float
+    } else {
+        hound::SampleFormat::Int
+    }
+}
+
+fn wav_spec_from_config(config: &cpal::SupportedStreamConfig) -> hound::WavSpec {
+    hound::WavSpec {
+        channels: config.channels() as _,
+        sample_rate: config.sample_rate().0 as _,
+        bits_per_sample: (config.sample_format().sample_size() * 8) as _,
+        sample_format: sample_format(config.sample_format()),
+    }
+}
+
+type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
+
+fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
+where
+    T: Sample,
+    U: Sample + hound::Sample + FromSample<T>,
+{
+    if let Ok(mut guard) = writer.try_lock() {
+        if let Some(writer) = guard.as_mut() {
+            for &sample in input.iter() {
+                let sample: U = U::from_sample(sample);
+                writer.write_sample(sample).ok();
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::time::Duration;
+
+use audio::record_wav;
+use enigo::{Direction, Enigo, Key, Keyboard};
+use tempfile::TempDir;
+use tokio::time::sleep;
+
+mod audio;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let dir = TempDir::new().expect("Failed to create temp dir");
+    let wav_path = dir.path().join("recording.wav");
+    record_wav(&wav_path).expect("Failed to record wav");
+    let audio_data = std::fs::read(wav_path).expect("Failed to read wav file");
+
+    dbg!(audio_data.len());
+
+    let client = reqwest::Client::new();
+    let request = client.post("http://127.0.0.1:8009/transcribe")
+        .body(audio_data)
+        .build()
+        .unwrap();
+
+    let text = match client.execute(request).await {
+        Ok(response) => response.text().await.expect("Failed to get text"),
+        Err(err) => {
+            eprintln!("Request failed {}", err);
+            return;
+        },
+    };
+
+    // Simulate typing
+    let mut enigo = Enigo::new(&Default::default()).unwrap();
+    for c in text.trim().chars() {
+        enigo.key(Key::Unicode(c), Direction::Click).expect("Failed to type key");
+        sleep(Duration::from_millis(5)).await;
+    }
+    enigo.key(Key::Space, Direction::Click).expect("Failed to type key");
+}
